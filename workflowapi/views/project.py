eb74771bc6d55118e943f95a61b176ab97cd93b8
pascual ramirez Valeriano <pascualramirez27@gmail.com>
2024-10-30 01:13:22 -0400

    created homepage, need services folder

--- workflowapi/views/project.py
+++ workflowapi/views/project.py
@@ -1,13 +1,19 @@
-from rest_framework import serializers, viewsets
+from rest_framework import serializers
+from rest_framework import viewsets
 from workflowapi.models import Project
-
+from .group import GroupSerializer  # Import the group serializer
+from .client import ClientSerializer  # If you have a separate client serializer
 
 class ProjectSerializer(serializers.HyperlinkedModelSerializer):
+    clientId = ClientSerializer(read_only=True)
+    assigned_group = GroupSerializer(read_only=True)  
+    
     class Meta:
         model = Project
-        fields = ('id', 'url', 'name', 'description', 'start_date', 'end_date')
-        
-        
+        fields = ('id', 'url', 'clientId', 'assigned_group', 'project_name', 
+                 'status', 'start_date', 'end_date', 'expected_duration')
+        depth = 1
+
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
--- workflowapi/views/projectWorker.py
+++ workflowapi/views/projectWorker.py
@@ -1,15 +1,15 @@
 from rest_framework import serializers
 from rest_framework import viewsets
 from workflowapi.models import ProjectWorker
+from .user import UserSerializer
 
 class ProjectWorkerSerializer(serializers.HyperlinkedModelSerializer):
+    worker = UserSerializer(source='worker_id', read_only=True)
     
     class Meta:
         model = ProjectWorker
-        fields = ('id', 'url', 'project', 'worker')
-        
-        
+        fields = ('id', 'url', 'project_id', 'worker_id', 'worker')
+
 class ProjectWorkerViewSet(viewsets.ModelViewSet):
-    
     queryset = ProjectWorker.objects.all()
     serializer_class = ProjectWorkerSerializer
